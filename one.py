03c698cab3784c6cd60da583dd04d50e079c786d
konathamnihari <konathamnihari@gmail.com>
2023-07-29 21:35:54 +0530

    python examples

---
+++ one.py
@@ -0,0 +1,17 @@
+# single line comment
+'''
+line1
+line2
+line3
+'''
+import math
+
+id=101
+ename="Nihari"
+
+def add():
+    pass
+
+class Emp:
+    pass
+
---
+++ two.py
@@ -0,0 +1,11 @@
+id9=101
+print(id1)
+
+9id=100
+print(9id)
+
+en@me="Nihari"
+print(en@me)
+
+class Emp:
+a=10
